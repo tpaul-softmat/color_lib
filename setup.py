52a237e9c0449a80dede1005d29066abc5387089
Tanay Paul <id4tanaypaul@gmail.com>
2025-04-21 15:16:24 -0500

    restructure

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 16:03:20 2025
+
+@author: tanay; Help: ChatGPT
+"""
+
+from setuptools import setup
+
+setup(
+    name="color_lib",
+    version="0.1",
+    py_modules=["color_palettes"],
+    install_requires=["matplotlib", "numpy"],
+)
